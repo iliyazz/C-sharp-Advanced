6179d257b61f807f01422d4faa22fe41cf7332c5
Iliya Zapryanov <iliyaz.softuni@gmail.com>
2022-05-24 07:59:42 +0300

    MultidimensionalArraysLab

---
+++ 05-Multidimensional Arrays/MultidimensionalArraysLab/1SumMatrixElements/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace _1SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[,] matrix = new int[matrixSize[0], matrixSize[1]];
+            ReadMatrix(matrix);
+            int sum = 0;
+            foreach (var item in matrix)
+            {
+                sum += item;
+            }
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+
+        private static void ReadMatrix(int[,] matrix)
+        {
+            const string separator = ", ";
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] inputData = Console.ReadLine().Split(separator).Select(int.Parse).ToArray();
+
+                for (int column = 0; column < matrix.GetLength(1); column++)
+                {
+                    matrix[row, column] = inputData[column];
+                }
+            }
+        }
+        //private static void ReadMatrix(string[,] matrix)
+        //{
+        //    const string separator = ", ";
+
+        //    for (int row = 0; row < matrix.GetLength(0); row++)
+        //    {
+        //        string[] inputData = Console.ReadLine().Split(separator).ToArray();
+
+        //        for (int column = 0; column < matrix.GetLength(1); column++)
+        //        {
+        //            matrix[row, column] = inputData[column];
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ 05-Multidimensional Arrays/MultidimensionalArraysLab/2SumMatrixColumns/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace _2SumMatrixColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[,] matrix = new int[matrixSize[0], matrixSize[1]];
+            ReadMatrix(matrix);
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sum += matrix[row, col];
+                }
+                Console.WriteLine(sum);
+            }
+        }
+        private static void ReadMatrix(int[,] matrix)
+        {
+            const string separator = " ";
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] inputData = Console.ReadLine().Split(separator).Select(int.Parse).ToArray();
+
+                for (int column = 0; column < matrix.GetLength(1); column++)
+                {
+                    matrix[row, column] = inputData[column];
+                }
+            }
+        }
+    }
+}
+        //private static void ReadMatrix(string[,] matrix)
+        //{
+        //    const string separator = ", ";
+
+        //    for (int row = 0; row < matrix.GetLength(0); row++)
+        //    {
+        //        string[] inputData = Console.ReadLine().Split(separator).ToArray();
+
+        //        for (int column = 0; column < matrix.GetLength(1); column++)
+        //        {
+        //            matrix[row, column] = inputData[column];
+        //        }
+        //    }
+        //}
