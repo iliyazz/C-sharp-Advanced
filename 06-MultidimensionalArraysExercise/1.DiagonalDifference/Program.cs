dcf721229c0e74123f02421842c0ac51f227335a
Iliya Zapryanov <iliyaz.softuni@gmail.com>
2022-05-24 12:02:14 +0300

    Diagonal Difference

---
+++ 06-MultidimensionalArraysExercise/1.DiagonalDifference/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.DiagonalDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            ReadMatrix(matrix);
+            int sumPrimaryDiagonal = 0;
+            int sumSecondaryDiagonal = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sumPrimaryDiagonal += matrix[i, i];
+                sumSecondaryDiagonal += matrix[i, n - i - 1];
+            }
+            Console.WriteLine(Math.Abs(sumPrimaryDiagonal - sumSecondaryDiagonal));
+        }
+        private static void ReadMatrix(int[,] matrix)
+        {
+            const string separator = " ";
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] inputData = Console.ReadLine().Split(separator).Select(int.Parse).ToArray();
+
+                for (int column = 0; column < matrix.GetLength(1); column++)
+                {
+                    matrix[row, column] = inputData[column];
+                }
+            }
+
+        }
+    }
+}
+//private static void ReadMatrix(string[,] matrix)
+//{
+//    const string separator = ", ";
+
+//    for (int row = 0; row < matrix.GetLength(0); row++)
+//    {
+//        string[] inputData = Console.ReadLine().Split(separator).ToArray();
+
+//        for (int column = 0; column < matrix.GetLength(1); column++)
+//        {
+//            matrix[row, column] = inputData[column];
+//        }
+//    }
+//}
+
