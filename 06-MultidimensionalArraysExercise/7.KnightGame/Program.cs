3f693fc243d5f02f2d615d741cec96bcbe52d31d
Iliya Zapryanov <iliyaz.softuni@gmail.com>
2022-05-24 22:28:27 +0300

    SnakeMoves KnightGame Bombs

---
+++ 06-MultidimensionalArraysExercise/7.KnightGame/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+
+namespace _7.KnightGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dimensions = int.Parse(Console.ReadLine());
+            string[,] chess = new string[dimensions, dimensions];
+            int sum = 0;
+            for (int row = 0; row < dimensions; row++)
+            {
+                string[] input = Console.ReadLine().ToCharArray().Select(x => x.ToString()).ToArray();
+                for (int col = 0; col < dimensions; col++)
+                {
+                    chess[row, col] = input[col];
+                }
+            }
+            int[] colisionCoord = new int[2];
+            while (MaxColision(chess, colisionCoord))
+            {
+                sum++;
+                chess[colisionCoord[0], colisionCoord[1]] = "0";
+            }
+            Console.WriteLine(sum);
+        }
+        static bool MaxColision(string[,] chess, int[] colisionCoord)
+        {
+            int maxLocal = 0;
+            int dim = chess.GetLength(0);
+            bool isColision = false;
+            for (int row = 0; row < dim; row++)
+            {
+                for (int col = 0; col < dim; col++)
+                {
+                    int sumLocal = 0;
+                    if (chess[row,col] == "K")
+                    {
+
+                        if (row - 2 >= 0 && col - 1 >= 0)
+                            if (chess[row - 2, col - 1] == "K")
+                                sumLocal++;
+                        if (row - 2 >= 0 && col + 1 < dim)
+                            if (chess[row - 2, col + 1] == "K")
+                                sumLocal++;
+                        if (row - 1 >= 0 && col + 2 < dim)
+                            if (chess[row - 1, col + 2] == "K")
+                                sumLocal++;
+                        if (row + 1 < dim && col + 2 < dim)
+                            if (chess[row + 1, col + 2] == "K")
+                                sumLocal++;
+                        if (row + 2 < dim && col + 1 < dim)
+                            if (chess[row + 2, col + 1] == "K")
+                                sumLocal++;
+                        if (row + 2 < dim && col - 1 >= 0)
+                            if (chess[row + 2, col - 1] == "K")
+                                sumLocal++;
+                        if (row + 1 < dim && col - 2 >= 0)
+                            if (chess[row + 1, col - 2] == "K")
+                                sumLocal++;
+                        if (row - 1 >= 0 && col - 2 >= 0)
+                            if (chess[row - 1, col - 2] == "K")
+                                sumLocal++;
+                        if (maxLocal < sumLocal)
+                        {
+                            maxLocal = sumLocal;
+                            colisionCoord[0] = row;
+                            colisionCoord[1] = col;
+                        }
+                        if (sumLocal != 0)
+                        {
+                            isColision = true;
+                        }
+                    }
+                }
+            }
+            return isColision;
+        }
+    }
+}
+
---
+++ 06-MultidimensionalArraysExercise/8.Bombs/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8.Bombs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dimension = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[dimension, dimension];
+            for (int row = 0; row < dimension; row++)
+            {
+                int[] value = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < dimension; col++)
+                {
+                    matrix[row, col] = value[col];
+                }
+            }
+            string[] firstCoord = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            Queue<Tuple<int, int>> bombCoord = new Queue<Tuple<int, int>>();
+            foreach (var item in firstCoord)
+            {
+                int[] coord = item.Split(",").Select(int.Parse).ToArray();
+                Tuple<int, int> current = Tuple.Create(coord[0], coord[1]);
+                bombCoord.Enqueue(current);
+            }
+            int numberOfBomb = bombCoord.Count;
+            for (int i = 0; i < numberOfBomb; i++)
+            {
+                int row = bombCoord.Peek().Item1;
+                int col = bombCoord.Peek().Item2;
+                if (matrix[row, col] > 0)
+                {
+
+                    if (row - 1 >= 0 && col - 1 >= 0)
+                        if (matrix[row - 1, col - 1] > 0)
+                            matrix[row - 1, col - 1] -= matrix[row, col];
+
+                    if (row - 1 >= 0)
+                        if (matrix[row - 1, col] > 0)
+                            matrix[row - 1, col] -= matrix[row, col];
+
+                    if (row - 1 >= 0 && col + 1 < dimension)
+                        if (matrix[row - 1, col + 1] > 0)
+                            matrix[row - 1, col + 1] -= matrix[row, col];
+
+                    if (col - 1 >= 0)
+                        if (matrix[row, col - 1] > 0)
+                            matrix[row, col - 1] -= matrix[row, col];
+
+                    if (col + 1 < dimension)
+                        if (matrix[row, col + 1] > 0)
+                            matrix[row, col + 1] -= matrix[row, col];
+
+                    if (row + 1 < dimension && col - 1 >= 0)
+                        if (matrix[row + 1, col - 1] > 0)
+                            matrix[row + 1, col - 1] -= matrix[row, col];
+
+                    if (row + 1 < dimension)
+                        if (matrix[row + 1, col] > 0)
+                            matrix[row + 1, col] -= matrix[row, col];
+
+                    if (row + 1 < dimension && col + 1 < dimension)
+                        if (matrix[row + 1, col + 1] > 0)
+                            matrix[row + 1, col + 1] -= matrix[row, col];
+                    matrix[row, col] = 0;
+                }
+                bombCoord.Dequeue();
+            }
+            int aliveCells = 0;
+            int sumOfCells = 0;
+            foreach (var item in matrix)
+            {
+                if (item > 0)
+                {
+                    aliveCells++;
+                    sumOfCells += item;
+                }
+            }
+            Console.WriteLine($"Alive cells: {aliveCells}");
+            Console.WriteLine($"Sum: {sumOfCells}");
+            for (int row = 0; row < dimension; row++)
+            {
+                for (int col = 0; col < dimension; col++)
+                {
+                    if (col < dimension - 1)
+                    {
+                        Console.Write($"{matrix[row, col]} ");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{matrix[row, col]}");
+                    }
+                }
+            }
+        }
+    }
+}
