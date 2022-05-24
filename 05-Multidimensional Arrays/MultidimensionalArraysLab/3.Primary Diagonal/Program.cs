89f0cc4a23ce65cef1e26301beec1a6bf20a128d
Iliya Zapryanov <iliyaz.softuni@gmail.com>
2022-05-24 09:09:29 +0300

    Primary Diagonal

--- 05-Multidimensional Arrays/MultidimensionalArraysLab/3.Primary Diagonal/Program.cs
+++ 05-Multidimensional Arrays/MultidimensionalArraysLab/3.Primary Diagonal/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _3.Primary_Diagonal
 {
@@ -6,7 +7,45 @@ namespace _3.Primary_Diagonal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            ReadMatrix(matrix);
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += matrix[i, i];
+            }
+            Console.WriteLine(sum);
+        }
+        private static void ReadMatrix(int[,] matrix)
+        {
+            const string separator = " ";
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] inputData = Console.ReadLine().Split(separator).Select(int.Parse).ToArray();
+
+                for (int column = 0; column < matrix.GetLength(1); column++)
+                {
+                    matrix[row, column] = inputData[column];
+                }
+            }
         }
     }
 }
+        //private static void ReadMatrix(string[,] matrix)
+        //{
+        //    const string separator = ", ";
+
+        //    for (int row = 0; row < matrix.GetLength(0); row++)
+        //    {
+        //        string[] inputData = Console.ReadLine().Split(separator).ToArray();
+
+        //        for (int column = 0; column < matrix.GetLength(1); column++)
+        //        {
+        //            matrix[row, column] = inputData[column];
+        //        }
+        //    }
+        //}
+
+
