9ddad525976e1051728188168f03b67a72a6d9ef
Iliya Zapryanov <iliyaz.softuni@gmail.com>
2022-05-28 15:19:45 +0300

    Sets and Dictionaries Advanced - Exercise Even Times

---
+++ 08-Sets and Dictionaries Advanced - Exercise/P04EvenTimes/P04EvenTimes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04EvenTimes
+{
+    internal class P04EvenTimes
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> evenTimes = new Dictionary<int, int>();
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (!evenTimes.ContainsKey(input))
+                    evenTimes.Add(input, 1);
+                else
+                evenTimes[input]++;
+            }
+            foreach (var item in evenTimes)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+
+        }
+    }
+}
