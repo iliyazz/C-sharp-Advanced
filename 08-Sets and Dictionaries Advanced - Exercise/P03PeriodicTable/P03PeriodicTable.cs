3722bf80d280c9bc0630bfab3ec6ee1258ad0906
Iliya Zapryanov <iliyaz.softuni@gmail.com>
2022-05-28 14:48:55 +0300

    Sets and Dictionaries Advanced - Exercise Periodic Table

---
+++ 08-Sets and Dictionaries Advanced - Exercise/P03PeriodicTable/P03PeriodicTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03PeriodicTable
+{
+    internal class P03PeriodicTable
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+            for (int i = 0; i < number; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                foreach (var item in input)
+                {
+                    set.Add(item);
+                }
+            }
+            Console.WriteLine(string.Join(" ", set.OrderBy(x => x)));
+        }
+    }
+}
