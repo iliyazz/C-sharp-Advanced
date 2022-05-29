a9c0e327d4ad61e8e473bc83bf2e9a6db911b1e3
Iliya Zapryanov <iliyaz.softuni@gmail.com>
2022-05-29 17:47:01 +0300

    Change .gitignore Streams, Files and Directories - Lab Odd Lines Line Numbers

---
+++ 09-Streams, Files and Directories - Lab/2.LineNumbers/LineNumbers.cs
@@ -0,0 +1,34 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                StreamWriter writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    int counter = 1;
+                    string line = string.Empty;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        counter++;
+                    }
+
+                }
+            }
+        }
+    }
+}
