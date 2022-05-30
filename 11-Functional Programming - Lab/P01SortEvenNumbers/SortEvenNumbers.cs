2fcba7f78649a581789160af73bfef598e2d5257
Iliya Zapryanov <iliyaz.softuni@gmail.com>
2022-05-30 22:50:25 +0300

    Functional Programming - Lab Sort Even Numbers

---
+++ 11-Functional Programming - Lab/P01SortEvenNumbers/SortEvenNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace P01SortEvenNumbers
+{
+    internal class SortEvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", Console.ReadLine().Split(", ").Select(x => int.Parse(x)).Where(x => x % 2 == 0).OrderBy(x => x)));
+        }
+    }
+}
