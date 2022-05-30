9140660a232befd93f2917d737f390965137449f
Iliya Zapryanov <iliyaz.softuni@gmail.com>
2022-05-30 23:19:18 +0300

    Functional Programming - Lab Sum Numbers

---
+++ 11-Functional Programming - Lab/P02SumNumbers/SumNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace P02SumNumbers
+{
+    internal class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Func<string, int> parser = x =>int.Parse(x);
+            int[] numbers = input.Split(", ").Select(parser).ToArray();
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
