9804e60feb261bbb3e04d340cce8ada413b8c3f8
iliyazz <100124829+iliyazz@users.noreply.github.com>
2022-06-01 08:20:32 +0300

    Functional Programming - Exercise
    Functional Programming - Exercise
    Knights of Honor

---
+++ 12-Functional Programming - Exercise/P02KnightsOfHonor/KnightsOfHonor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02KnightsOfHonor
+{
+    internal class KnightsOfHonor
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+            //Func<string, string> printNamesFunc = name => "Sir " + name;
+            //names.ForEach(x => Console.WriteLine(printNamesFunc(x)));
+            Action<string> printNames = name => Console.WriteLine($"Sir { name}");
+            names.ForEach(printNames);
+        }
+    }
+}
