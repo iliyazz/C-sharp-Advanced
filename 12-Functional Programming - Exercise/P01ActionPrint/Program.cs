11cf78cf3705ea77e9f821a270e7f4b09200a62d
iliyazz <100124829+iliyazz@users.noreply.github.com>
2022-06-01 07:56:14 +0300

    Functional Programming - Exercise
    Functional Programming - Exercise
    Action Print

---
+++ 12-Functional Programming - Exercise/P01ActionPrint/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+            Action<string> printNames =  name => Console.WriteLine(name);
+            names.ForEach(printNames);
+        }
+    }
+}
