e3fb3c6b0a1d6d4620ad2d9aa7ccf4bad960f07f
iliyazz <iliyaz.softuni@gmail.com>
2022-06-03 00:55:10 +0300

    Functional Programming - Exercise TriFunction

---
+++ 12-Functional Programming - Exercise/P11TriFunction/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P11TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+            Console.WriteLine(names.First(x => x.Select(y => (int) y).Sum() >= number));
+        }
+    }
+}
