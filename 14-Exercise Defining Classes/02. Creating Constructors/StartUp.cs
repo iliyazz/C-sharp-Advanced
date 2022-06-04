58c0b2d9744fc944de322d52ecdc87d36d158807
iliyazz <iliyaz.softuni@gmail.com>
2022-06-04 10:43:43 +0300

    Defining Classes - Exercise, Creating Constructors

---
+++ 14-Exercise Defining Classes/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Peter", 20);
+            Person person2 = new Person("George", 18);
+            Person person3 = new Person("Jose", 43);
+        }
+    }
+}
