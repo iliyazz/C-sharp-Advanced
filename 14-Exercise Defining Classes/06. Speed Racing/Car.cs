a22934401ee78fca06f41ee2a901fc02a67b5b3b
iliyazz <iliyaz.softuni@gmail.com>
2022-06-04 14:36:14 +0300

    Defining Classes - Exercise, Speed Racing

---
+++ 14-Exercise Defining Classes/06. Speed Racing/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance;
+
+        public string Model { get { return model; } set { model = value; } }
+        public double FuelAmount { get { return fuelAmount; } set { fuelAmount = value; } }
+        public double FuelConsumptionPerKilometer { get { return fuelConsumptionPerKilometer; } set { fuelConsumptionPerKilometer = value; } }
+
+        public double TravelledDistance { get => travelledDistance; set => travelledDistance = value; }
+
+        public Car()
+        {
+            this.travelledDistance = 0;
+        }
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer, double travelledDistance)
+            :this()
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            this.TravelledDistance = travelledDistance;
+            
+        }
+        public void Drive(string command, Dictionary<string, Car> cars)
+        
+        {
+            string[] cmdArg = command.Split();
+            string carModel = cmdArg[1];
+            int amountOfKm = int.Parse(cmdArg[2]);
+            double consumtion = amountOfKm * cars[carModel].FuelConsumptionPerKilometer;
+            if (cars[carModel].FuelAmount >= consumtion)
+            {
+                cars[carModel].FuelAmount -= consumtion;
+                cars[carModel].travelledDistance += amountOfKm;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ 14-Exercise Defining Classes/06. Speed Racing/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Car> cars = new Dictionary<string, Car>();
+            int numberOfCar = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfCar; i++)
+            {
+                Car car = new Car();
+                string[] carData = Console.ReadLine().Split();
+                car.Model = carData[0];
+                car.FuelAmount = double.Parse(carData[1]);
+                car.FuelConsumptionPerKilometer = double.Parse(carData[2]);
+                cars.Add(car.Model, car);
+            }
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                Car car = new Car();
+                car.Drive(command, cars);
+            }
+            foreach (var item in cars)
+            {
+                Console.WriteLine($"{item.Value.Model} {item.Value.FuelAmount:f2} {item.Value.TravelledDistance}");
+            }
+        }
+    }
+}
