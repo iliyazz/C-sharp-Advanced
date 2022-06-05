09771fbca930ec8d7e0f92fd8d8f8fa381e1bb49
iliyazz <iliyaz.softuni@gmail.com>
2022-06-05 14:53:49 +0300

    Defining Classes - Exercise, SoftUni Parking

---
+++ 14-Exercise Defining Classes/10. SoftUni Parking/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+        public string Make { get { return make; } set { make = value; } }
+        public string Model { get { return model; } set { model = value; } }
+        public int HorsePower { get { return horsePower; } set { horsePower = value; } }
+        public string RegistrationNumber { get { return registrationNumber; } set { registrationNumber = value; } }
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+            RegistrationNumber = registrationNumber;
+        }
+        public override string ToString()
+        {
+
+            string result = $"Make: {this.Make}{Environment.NewLine}";
+            result += $"Model: {this.Model}{Environment.NewLine}";
+            result += $"HorsePower: {this.HorsePower}{Environment.NewLine}";
+            result += $"RegistrationNumber: {this.RegistrationNumber}";
+
+            return result;
+
+            //StringBuilder sb = new StringBuilder();
+            //sb.Append($"Make: {Make}{Environment.NewLine}");
+            //sb.Append($"Model: {Model}{Environment.NewLine}");
+            //sb.Append($"HoursePower: {HoursePower}{Environment.NewLine}");
+            //sb.Append($"RegistrationNumber: {RegistrationNumber}");
+            //return sb.ToString();
+        }
+    }
+}
---
+++ 14-Exercise Defining Classes/10. SoftUni Parking/Parking.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private List<Car> cars;
+        private int capacity;
+
+        public List<Car> Cars { get { return cars; } set {cars = value; } }
+        public int Capacity { get { return capacity; } set { capacity = value; } }
+        public int Count { get { return cars.Count; } }
+        public Parking(int capacity)
+        {
+            Capacity = capacity;
+            Cars = new List<Car>();
+        }
+        public Car GetCar(string regNumber)
+        {
+            return Cars.First(x => x.RegistrationNumber == regNumber);
+        }
+        public void RemoveSetOfRegistrationNumber(List<string> regNumbers)
+        {
+            foreach (var item in regNumbers)
+            {
+                RemoveCar(item);
+            }
+        }
+
+        public string RemoveCar(string regNumber)
+        {
+            bool isExist = false;
+            foreach (Car item in Cars)
+            {
+                if (item.RegistrationNumber == regNumber)
+                {
+                    isExist = true;
+                }
+            }
+            if (!isExist)
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+            else
+            {
+                Car carToRemove = Cars.First(x => x.RegistrationNumber == regNumber);
+                cars.Remove(carToRemove);
+                return $"Successfully removed {regNumber}";
+            }
+        }
+        public string AddCar(Car addedCar)
+        {
+            bool isPossibleToAddCar = true;
+            foreach (Car item in Cars)
+            {
+                if (item.RegistrationNumber == addedCar.RegistrationNumber)
+                {
+                    isPossibleToAddCar = false;
+                    return "Car with that registration number, already exists!";
+                }
+            }
+
+            if (Cars.Count + 1 > Capacity)
+            {
+                //isPossibleToAddCar = false;
+                return "Parking is full!";
+            }
+
+            if (isPossibleToAddCar)
+            {
+                Cars.Add(addedCar);
+                return $"Successfully added new car {addedCar.Make} {addedCar.RegistrationNumber}";
+            }
+            return "";
+        }
+    }
+}
