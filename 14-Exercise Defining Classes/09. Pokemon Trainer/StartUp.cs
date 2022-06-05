579bf0109a2a021863997338e20e02d08e3876a7
iliyazz <iliyaz.softuni@gmail.com>
2022-06-05 11:22:27 +0300

    Defining Classes - Exercise, Pokemon Trainer

---
+++ 14-Exercise Defining Classes/09. Pokemon Trainer/StartUp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string token = string.Empty;
+            List<Trainer> trainers = new List<Trainer>();
+
+            while ((token = Console.ReadLine()) != "Tournament")
+            {
+                string[] tokens = token.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string trainerName = tokens[0];
+                string pokemonName = tokens[1];
+                string pokemonElement = tokens[2];
+                int pokemonHealth = int.Parse(tokens[3]);
+                Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+                Trainer trainer = trainers.FirstOrDefault(x => x.Name == trainerName);
+                if (trainer != null)
+                {
+                    trainer.CollectionOfPokemon.Add(pokemon);
+                }
+                else
+                {
+                    Trainer newTrainer = new Trainer(trainerName);
+                    newTrainer.CollectionOfPokemon.Add(pokemon);
+                    trainers.Add(newTrainer);
+                }
+            }
+            
+            while ((token = Console.ReadLine()) != "End")
+            {
+                for (int i = 0; i < trainers.Count; i++)
+                {
+                    if (trainers[i].CollectionOfPokemon.Any(x => x.Element == token))
+                    {
+                        trainers[i].NumberOfBadges++;
+                    }
+                    else
+                    {
+                        foreach (var pokemon in trainers[i].CollectionOfPokemon)
+                        {
+                            pokemon.ReduceHealth();
+                        }
+                        trainers[i].CollectionOfPokemon.RemoveAll(x => x.Health <= 0);
+                    }
+                }
+            }
+            foreach (var item in trainers.OrderByDescending(x => x.NumberOfBadges))
+            {
+                Console.WriteLine($"{item.Name} {item.NumberOfBadges} {item.CollectionOfPokemon.Count}");
+            }
+        }
+    }
+}
---
+++ 14-Exercise Defining Classes/09. Pokemon Trainer/Trainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Trainer
+    {
+        private string name;
+        private int numberOfBadges;
+        private List<Pokemon> collectionOfPokemon;
+
+        public string Name { get { return this.name; } set { name = value; } }
+        public int NumberOfBadges { get { return numberOfBadges; } set {numberOfBadges = value; } }
+        public List<Pokemon> CollectionOfPokemon { get { return collectionOfPokemon; } set { collectionOfPokemon = value; } }
+        
+        public Trainer(string name)
+        {
+            Name = name;
+            NumberOfBadges = 0;
+            CollectionOfPokemon = new List<Pokemon>();
+        }
+    }
+}
