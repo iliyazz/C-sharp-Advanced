297e1ad71c2b084a9fc354bcdcb9d6052d5f01b0
iliyazz <100124829+iliyazz@users.noreply.github.com>
2022-06-06 13:27:36 +0300

    C# Advanced Retake Exam - 13 April 2022, Meal Plan

---
+++ 00-Exams as Exercises/Exam Retake 13.04.2022/P01MealPlan/MealPlan.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P01MealPlan
+{
+    class MealPlan
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> meals = new Queue<string>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries));
+            Stack<int> caloriesPerDay = new Stack<int>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            int mealsNumber = meals.Count;
+
+            while ((meals.Count > 0) && (caloriesPerDay.Count > 0))
+            {
+                int currentDayCal = caloriesPerDay.Pop();
+
+                int currentMealCal = CaloriesFromMeal(meals.Peek());
+
+                if (currentDayCal - currentMealCal >= 0)
+                {
+                    currentDayCal -= currentMealCal;
+                    meals.Dequeue();
+                    caloriesPerDay.Push(currentDayCal);
+                }
+                else if (caloriesPerDay.Count > 0)
+                {
+                    currentDayCal -= currentMealCal;
+                    currentDayCal += caloriesPerDay.Pop();
+                    if (currentDayCal > 0)
+                    {
+                        caloriesPerDay.Push(currentDayCal);
+                        meals.Dequeue();
+                    }
+
+                    if (caloriesPerDay.Count == 0)
+                    {
+                        meals.Dequeue();
+                    }
+                }
+                else
+                {
+                    if (caloriesPerDay.Count == 0)
+                    {
+                        meals.Dequeue();
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            if (caloriesPerDay.Count > 0)
+            {
+                sb.AppendLine($"John had {mealsNumber} meals.");
+                sb.AppendLine($"For the next few days, he can eat {string.Join(", ", caloriesPerDay)} calories.");
+            }
+            else
+            {
+                sb.AppendLine($"John ate enough, he had {mealsNumber - meals.Count} meals.");
+                sb.AppendLine($"Meals left: {string.Join(", ", meals)}.");
+
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+        private static int CaloriesFromMeal(string currentMeal)
+        {
+            const int saladCalories = 350;
+            const int soupCalories = 490;
+            const int pastaCalories = 680;
+            const int steakCalories = 790;
+            if (currentMeal == "salad")
+            {
+                return saladCalories;
+            }
+            else if (currentMeal == "soup")
+            {
+                return soupCalories;
+            }
+            else if (currentMeal == "pasta")
+            {
+                return pastaCalories;
+            }
+            else if (currentMeal == "steak")
+            {
+                return steakCalories;
+            }
+            return 0;
+        }
+
+    }
+}
