6e7c290d866decd98cbad6f3d833811833e18e7b
iliyazz <iliyaz.softuni@gmail.com>
2022-06-11 08:32:20 +0300

    Generics - Lab, Array Creator

---
+++ 17-Generics - Lab/P02ArrayCreator/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public static class ArrayCreator
+    {
+        public static T[] Create<T>(int lenght, T item)
+        {
+            var result = new T[lenght];
+            for (int i = 0; i < lenght; i++)
+            {
+                result[i] = item;
+            }
+            return result;
+        }
+    }
+}
