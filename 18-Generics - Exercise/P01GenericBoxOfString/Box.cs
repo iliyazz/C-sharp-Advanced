4d5a22b410076ecd9ca88722fec887d396cc60b5
iliyazz <iliyaz.softuni@gmail.com>
2022-06-11 08:39:15 +0300

    Generics - Exercise, Generic Box of String

---
+++ 18-Generics - Exercise/P01GenericBoxOfString/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01GenericBoxOfString
+{
+    public class Box<T>
+    {
+
+        //constructor
+        public Box(T element)
+        {
+            Element = element;
+        }
+        //property
+        public T Element { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {Element}";
+        }
+    }
+}
---
+++ 18-Generics - Exercise/P01GenericBoxOfString/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                var input = Console.ReadLine();
+                var box = new Box<string>(input);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
