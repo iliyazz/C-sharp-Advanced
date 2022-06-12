89b87061e468db56123a442cfd1d9885c5a31c46
iliyazz <iliyaz.softuni@gmail.com>
2022-06-12 19:50:29 +0300

    Generics - Exercise,

---
+++ 18-Generics - Exercise/P07Tuple/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Box
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input1 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var tuple1 = new Tuple<string, string>($"{input1[0]} {input1[1]}", input1[2]);
+
+
+            string[] input2 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var tuple2 = new Tuple<string, int>(input2[0], int.Parse(input2[1]));
+
+            string[] input3 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var tuple3 = new Tuple<int, double>(int.Parse(input3[0]), double.Parse(input3[1]));
+
+            Console.WriteLine(tuple1);
+            Console.WriteLine(tuple2);
+            Console.WriteLine(tuple3);
+        }
+    }
+}
---
+++ 18-Generics - Exercise/P07Tuple/Tuple1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Box
+{
+    public class Tuple<T1, T2>
+    {
+        public Tuple(T1 element1, T2 element2)
+        {
+            Element1 = element1;
+            Element2 = element2;
+        }
+        public T1 Element1 { get; private set;}
+        public T2 Element2 { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Element1} -> {Element2}";
+        }
+    }
+}
