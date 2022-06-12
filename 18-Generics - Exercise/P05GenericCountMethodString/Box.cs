72df091e5f6c29d072bb7b08ae5e64fae5f93071
iliyazz <iliyaz.softuni@gmail.com>
2022-06-12 17:52:59 +0300

    Generics - Exercise, Generic Count Method String

---
+++ 18-Generics - Exercise/P05GenericCountMethodString/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace Box
+{
+    public class Box<T> : IComparable<T> where T : IComparable //: IComparable<T> where T :IComparable<T>
+    {
+        public Box(T element)
+        {
+            Element = element;
+        }
+        public Box(List<T> elements)
+        {
+            Elements = elements;
+        }
+        public T Element { get; private set; }
+        public List<T> Elements { get; private set; }
+
+        public int CompareTo(T other)
+            => Element.CompareTo(other);
+        public int CountOfElements<T>(List<T> list, T readline) where T : IComparable => list.Count(word => word.CompareTo(readline) > 0);
+
+    }
+}
---
+++ 18-Generics - Exercise/P05GenericCountMethodString/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            var list = new List<string>();
+            for (int i = 0; i < number; i++)
+            {
+                var input = Console.ReadLine();
+                list.Add(input);
+            }
+            var box = new Box<string>(list);
+            var elementToCompare = Console.ReadLine();
+            var count = box.CountOfElements(list, elementToCompare);
+            Console.WriteLine(count);
+        }
+    }
+}
