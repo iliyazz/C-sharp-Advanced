d9da1a9e59e783adf4362f64c125c4fce64dc8cc
iliyazz <iliyaz.softuni@gmail.com>
2022-06-12 20:01:13 +0300

    Generics - Exercise, Threeuple

---
+++ 18-Generics - Exercise/P08Threeuple/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Box
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input1 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string town = input1[3];
+            for (int i = 4; i < input1.Length; i++)
+            {
+                town += " " + input1[i];
+            }
+            var threeuple1 = new Threeuple<string, string, string>($"{input1[0]} {input1[1]}", input1[2], town);
+
+            string[] input2 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            bool IsDrunked = input2[2] == "drunk" ? true : false;
+            
+            var threeuple2 = new Threeuple<string, int, bool>(input2[0], int.Parse(input2[1]), IsDrunked);
+
+            string[] input3 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var threeuple3 = new Threeuple<string, double, string>(input3[0], double.Parse(input3[1]), input3[2]);
+
+            Console.WriteLine(threeuple1);
+            Console.WriteLine(threeuple2);
+            Console.WriteLine(threeuple3);
+        }
+    }
+}
---
+++ 18-Generics - Exercise/P08Threeuple/Threeuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Box
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        public Threeuple(T1 element1, T2 element2, T3 element3)
+        {
+            Element1 = element1;
+            Element2 = element2;
+            Element3 = element3;
+        }
+        public T1 Element1 { get; private set;}
+        public T2 Element2 { get; private set; }
+        public T3 Element3 { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Element1} -> {Element2} -> {Element3}";
+        }
+    }
+}
