959d92ede35b837ff032ddc30a798428bbffe94e
iliyazz <iliyaz.softuni@gmail.com>
2022-06-12 18:05:20 +0300

    Generics - Exercise, Generic Count Method Double

---
+++ 18-Generics - Exercise/P06GenericCountMethodDouble/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace Box
+{
+    public class Box<T> : IComparable<T> where T : IComparable 
+    {
+        public Box(T element)
+        {
+            Element = element;
+        }
+        public Box(List<T> elements)
+        {
+            Elements = elements;
+        }
+        public T Element { get; private set; }
+        public List<T> Elements { get; private set; }
+
+        public int CountOfElements<T1>(List<T1> list, T1 readline) where T1 : IComparable => list.Count(word => word.CompareTo(readline) > 0);
+        public int CompareTo(T other) => Element.CompareTo(other);
+
+    }
+}
---
+++ 18-Generics - Exercise/P06GenericCountMethodDouble/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            var list = new List<double>();
+            for (int i = 0; i < number; i++)
+            {
+                var input = double.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+            var box = new Box<double>(list);
+            var elementToCompare = double.Parse(Console.ReadLine());
+            var count = box.CountOfElements(list, elementToCompare);
+            Console.WriteLine(count);
+        }
+    }
+}
