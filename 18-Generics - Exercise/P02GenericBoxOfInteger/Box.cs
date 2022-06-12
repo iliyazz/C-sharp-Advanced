372ebdc77eb2bfe1468ee8c6cc7300e913d93380
iliyazz <iliyaz.softuni@gmail.com>
2022-06-12 16:15:19 +0300

    C# Advanced - Exercises, Generic Box of Integer

---
+++ 18-Generics - Exercise/P02GenericBoxOfInteger/Box.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P02GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        public Box(T element)
+        {
+            Element = element;
+        }
+        public T Element { get; private set; }
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {Element}";
+        }
+    }
+}
---
+++ 18-Generics - Exercise/P02GenericBoxOfInteger/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P02GenericBoxOfInteger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                var data = new Box<int>(input);
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
