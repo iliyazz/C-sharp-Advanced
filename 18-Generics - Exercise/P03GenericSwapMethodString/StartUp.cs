2f9c55090432abd11373673a438d9e20f09874f3
iliyazz <iliyaz.softuni@gmail.com>
2022-06-12 16:41:33 +0300

    Generics - Exercise, Generic Swap Method String

---
+++ 18-Generics - Exercise/P03GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            var list = new List<string>();
+            for (int i = 0; i < number; i++)
+            {
+                var input = Console.ReadLine();
+                list.Add(input);
+            }
+            var box = new Box<string>(list);
+            var indexes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            box.Swap(list, indexes[0], indexes[1]);
+            Console.WriteLine(box);
+        }
+    }
+}
