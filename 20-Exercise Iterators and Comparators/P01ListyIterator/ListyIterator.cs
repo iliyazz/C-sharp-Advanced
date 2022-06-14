2a9950c1f678e278b9fff8f070499415b08e53c5
iliyazz <iliyaz.softuni@gmail.com>
2022-06-14 23:11:25 +0300

    Iterators and Comparators - Exercise, ListyIterator

---
+++ 20-Exercise Iterators and Comparators/P01ListyIterator/ListyIterator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+        private int index;
+        public ListyIterator(params T[] data)
+        {
+            collection = new List<T>(data);
+            index = 0;
+        }
+        public bool HasNext() => index < collection.Count - 1;
+        public bool Move()
+        {
+            bool canMove = HasNext();
+            if (canMove)
+            {
+                index++;
+            }
+            return canMove;
+        }
+        public void Print()
+        {
+            if (collection.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+            Console.WriteLine($"{collection[index]}");
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in collection)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ 20-Exercise Iterators and Comparators/P01ListyIterator/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace P01ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+            ListyIterator<string> iteratorList = null;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var cmdArg = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (cmdArg[0] == "Create")
+                {
+                    iteratorList = new ListyIterator<string>(cmdArg.Skip(1).ToArray());
+                }
+                else if (cmdArg[0] == "Move")
+                {
+                    Console.WriteLine(iteratorList.Move());
+                }
+                else if (cmdArg[0] == "Print")
+                {
+                    iteratorList.Print();
+                }
+                else if (cmdArg[0] == "HasNext")
+                {
+                    Console.WriteLine(iteratorList.HasNext());
+                }
+            }
+
+        }
+    }
+}
