8e9a850e85d085aae94fa5cbadc5286cfcb9534f
iliyazz <iliyaz.softuni@gmail.com>
2022-06-14 23:39:16 +0300

    Iterators and Comparators - Exercise, Froggy

---
+++ 20-Exercise Iterators and Comparators/P04Froggy/StarUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace P04Froggy
+{
+    public class StarUp
+    {
+        static void Main(string[] args)
+        {
+            var lake = new Lake(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
