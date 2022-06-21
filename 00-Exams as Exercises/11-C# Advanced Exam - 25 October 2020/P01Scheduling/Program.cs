8ba89e148aad639ea99f254c0aaacda42d5525df
iliyazz <iliyaz.softuni@gmail.com>
2022-06-21 22:23:06 +0300

    C# Advanced Exam - 25 October 2020, Scheduling

---
+++ 00-Exams as Exercises/11-C# Advanced Exam - 25 October 2020/P01Scheduling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01Scheduling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> tasks = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Queue<int> threads = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            int taskToBeKilled = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                int thread = threads.Peek();
+                int task = tasks.Peek();
+                if (task == taskToBeKilled)
+                {
+                    break;
+                }
+                else if (thread >= task)
+                {
+                    threads.Dequeue();
+                    tasks.Pop();
+                }
+                else 
+                {
+                    threads.Dequeue();
+                }
+            }
+            Console.WriteLine($"Thread with value {threads.Peek()} killed task {taskToBeKilled}");
+            Console.WriteLine(string.Join(" ", threads));
+            
+        }
+    }
+}
