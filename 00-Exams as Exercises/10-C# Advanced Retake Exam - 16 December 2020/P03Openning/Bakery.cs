ba457f1ba1c1fd0e55a6078baa5d890d97d71eed
iliyazz <100124829+iliyazz@users.noreply.github.com>
2022-06-21 17:03:54 +0300

    C# Advanced Retake Exam - 16 December 2020, Openning

---
+++ 00-Exams as Exercises/10-C# Advanced Retake Exam - 16 December 2020/P03Openning/Bakery.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private List<Employee> data;
+        private string name;
+        private int capacity;
+
+        public string Name { get => name; set => name = value; }
+        public int Capacity { get => capacity; set => capacity = value; }
+        public List<Employee> Data { get => data; set => data = value; }
+        public int Count{ get => data.Count; }
+        public Bakery(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Data = new List<Employee>();
+        }
+        public void Add(Employee employee)
+        {
+            if(data.Count < capacity) { data.Add(employee); }
+        }
+        public bool Remove(string name)
+        {
+            Employee current = data.FirstOrDefault(e => e.Name == name);
+            if(current == null) { return false; }
+            data.Remove(current);
+            return true;
+        }
+        public Employee GetOldestEmployee()
+        {
+            Employee current = Data.OrderBy(x => x.Age).Last();
+            return current;
+        }
+        public Employee GetEmployee(string name)
+        {
+            Employee current = Data.FirstOrDefault(x => x.Name == name);
+            return current;
+        }
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Employees working at Bakery {Name}")
+                .Append(string.Join(Environment.NewLine, Data));
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 00-Exams as Exercises/10-C# Advanced Retake Exam - 16 December 2020/P03Openning/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Employee
+    {
+        private string name;
+        private int age;
+        private string country;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public string Country { get => country; set => country = value; }
+
+        public Employee(string name, int age, string country)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+        }
+        public override string ToString()
+        {
+            return $"Employee: {name}, {age} ({country})";
+        }
+    }
+}
