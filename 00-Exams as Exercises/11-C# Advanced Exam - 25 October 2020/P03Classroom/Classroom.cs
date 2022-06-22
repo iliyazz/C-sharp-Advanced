c35246b02c7969074d7b344d341dcb834ad77593
iliyazz <100124829+iliyazz@users.noreply.github.com>
2022-06-22 08:13:48 +0300

    C# Advanced Exam - 25 October 2020, Classroom

---
+++ 00-Exams as Exercises/11-C# Advanced Exam - 25 October 2020/P03Classroom/Classroom.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+        private int capacity;
+
+        public List<Student> Students { get => students; set => students = value; }
+        public int Capacity { get => capacity; set => capacity = value; }
+        public int Count => students.Count;
+        public Classroom(int capacity)
+        {
+            Capacity = capacity;
+            students = new List<Student>();
+        }
+        public string RegisterStudent(Student student)
+        {
+            if (Students.Count < Capacity)
+            {
+                Students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            return "No seats in the classroom";
+        }
+        public string DismissStudent(string firstName, string lastName)
+        {
+            Student student = Students.Where(x => x.FirstName == firstName && x.LastName == lastName).FirstOrDefault();
+            if (student != null)
+            {
+                students.Remove(student);
+                return $"Dismissed student {firstName} {lastName}";
+            }
+            return "Student not found";
+        }
+        public string GetSubjectInfo(string subject)
+        {
+            List<Student> studentsBySubject = new List<Student>();
+            studentsBySubject = Students.Where(x => x.Subject == subject).ToList();
+            if (studentsBySubject.Count == 0)
+            {
+                return "No students enrolled for the subject";
+            }
+            var sb = new StringBuilder();
+            sb.Append($"Subject: {subject}{Environment.NewLine}Students:");
+           
+            foreach (var item in studentsBySubject)
+            {
+                sb.Append($"{Environment.NewLine}{item.FirstName} {item.LastName}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+        public int GetStudentsCount()
+        {
+            return Students.Count;
+        }
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student student = Students.Where((x) => x.FirstName == firstName && x.LastName == lastName).FirstOrDefault();
+            return student;
+        }
+    }
+}
---
+++ 00-Exams as Exercises/11-C# Advanced Exam - 25 October 2020/P03Classroom/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        private string subject;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Subject { get => subject; set => subject = value; }
+
+        public Student(string firstName, string lastName, string subject)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Subject = subject;
+        }
+        public override string ToString()
+        {
+            return $"Student: First Name = {firstName}, Last Name = {lastName}, Subject = {subject}";
+        }
+    }
+}
