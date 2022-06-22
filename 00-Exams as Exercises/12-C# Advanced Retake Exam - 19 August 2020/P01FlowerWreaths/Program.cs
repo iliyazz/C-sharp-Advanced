f498a9ac883a0489b76324121f95b0e8eab5f6c1
iliyazz <iliyaz.softuni@gmail.com>
2022-06-22 18:42:27 +0300

    C# Advanced Retake Exam - 19 August 2020, Flower Wreaths

---
+++ 00-Exams as Exercises/12-C# Advanced Retake Exam - 19 August 2020/P01FlowerWreaths/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01FlowerWreaths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> lilies = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Queue<int> roses = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            int sumForLaterUse = 0;
+            int wreaths = 0;
+            while (lilies.Count > 0 && roses.Count > 0)
+            {
+                int lily = lilies.Pop();
+                int rose = roses.Dequeue();
+                if (lily + rose == 15)
+                {
+                    wreaths++;
+                }
+                else if (lily + rose > 15)
+                {
+                    while (lily + rose > 15)
+                    {
+                        lily -= 2;
+                        if (lily + rose == 15)
+                        {
+                            wreaths++;
+                        }
+                    }
+                    if (lily + rose < 15)
+                    {
+                        sumForLaterUse += lily + rose;
+                    }
+                }
+                else
+                {
+                    sumForLaterUse += lily + rose;
+                }
+            }
+            wreaths += sumForLaterUse / 15;
+            if (wreaths >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreaths} wreaths more!");
+            }
+        }
+    }
+}
